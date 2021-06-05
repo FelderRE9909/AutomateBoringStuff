c61944a4dd81d3a89e868fba36661d8e47e2e80b
FelderRE9909 <ron.felder@gmail.com>
2021-06-05 12:03:06 +0000

    added new file and updated gitpod.yml

--- emailandphone.py
+++ emailandphone.py
@@ -1,4 +1,4 @@
-“#! python3
+#! python3
 # phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard.
 
 import pyperclip, re
@@ -12,36 +12,34 @@ phoneRegex = re.compile(r'''(
     (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
     )''', re.VERBOSE)
 
-# TODO: Create email regex.
 # Create email regex.
 emailRegex = re.compile(r'''(
-  ➊ [a-zA-Z0-9._%+-]+      # username
-  ➋ @                      # @ symbol
-  ➌ [a-zA-Z0-9.-]+         # domain name
-    (\.[a-zA-Z]{2,4})       # dot-something
-    )''', re.VERBOSE)”
+  [a-zA-Z0-9._%+-]+      # username
+  @                      # @ symbol
+  [a-zA-Z0-9.-]+         # domain name
+  (\.[a-zA-Z]{2,4})       # dot-something
+  )''', re.VERBOSE)
 
-# TODO: Find matches in clipboard text.
 # Find matches in clipboard text.
-   text = str(pyperclip.paste())
+text = str(pyperclip.paste())
 
-➊ matches = []
-➋ for groups in phoneRegex.findall(text):
-       phoneNum = '-'.join([groups[1], groups[3], groups[5]])
-       if groups[8] != '':
-           phoneNum += ' x' + groups[8]
-       matches.append(phoneNum)
-➌ for groups in emailRegex.findall(text):
-       matches.append(groups[0])”
+matches = []
+for groups in phoneRegex.findall(text):
+    phonenum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phonenum += ' x' + groups[8]
+        matches.append(phonenum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
 
 # TODO: Copy results to the clipboard.
 # Copy results to the clipboard.
 if len(matches) > 0:
-    pyperclip.copy('\n'.join(matches))
-    print('Copied to clipboard:')
-    print('\n'.join(matches))
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
 else:
-    print('No phone numbers or email addresses found.')”
+    print('No phone numbers or email addresses found.')
 
 
 # Program From
